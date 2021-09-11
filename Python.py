e369caa9ca873a734ac7b6306817c10215b419a9
Hangjust <87064605+Hangjust@users.noreply.github.com>
2021-09-12 00:14:40 +0200

    Create Python.py

---
+++ Python.py
@@ -0,0 +1,35 @@
+#imports
+
+import time
+import random
+
+
+
+
+#-------------------------
+
+
+name = input("Enter Your name: ")
+print("hello " + name)
+time.sleep (1)
+
+age = int(input("What is your age?"))
+
+time.sleep(1)
+
+print("nice to meet you " + str(name))
+
+time.sleep(2)
+
+current_year = 2021
+birth_year = current_year - age
+
+print("So you are born in " + str(birth_year))
+
+time.sleep(1)
+
+print("lets play a game!!!")
+time.sleep(1)
+
+loading = print(loading.) sleep.time(0.1)
+laoding * 20
