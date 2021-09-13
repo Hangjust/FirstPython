a42dcdce0f33151a2673d044be71536f330b547c
Hangjust <87064605+Hangjust@users.noreply.github.com>
2021-09-13 10:52:19 +0200

    Create canculator.py

---
+++ canculator.py
@@ -0,0 +1,36 @@
+
+
+turk = ()
+
+what = int(input("what do you want +, -, x, : ?"))
+
+if what == (":"):
+    turk = (/)
+elif:
+    what == ("*")
+    turk = x
+elif:
+    what == ("+")
+    turk = +
+else:
+    what == ("-")
+    turk = -
+
+number1 = int(input("put a number here "))
+number2 = int(input("put another number "))
+number3 = int(input("put 3rd number"))
+
+
+x = (number1 turk number2)
+
+print (x)
+
+
+sbhhgvhu
+
+turk = x.__add__    ####
+
+
+
+
+
